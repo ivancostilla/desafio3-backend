5e4822e550b61b6fb7e9385e09857378da84238b
ivancostilla <ivan20punto88@gmail.com>
2021-10-14 05:23:08 -0300

    agrego async

--- server.js
+++ server.js
@@ -1,93 +1,80 @@
+
 const express = require("express");
-const fs = require('fs');
+const fs = require('fs/promises');
 const app = express();
 const PORT = 8080;
-
 class Contenedor {
-  constructor(nombreArchivo) {
-    this.file = nombreArchivo;
-  }
-  save(obj) {
-    try {
-      const data = JSON.parse(fs.readFileSync(this.file, "utf-8"));
-      if (data.length === 0) {
-        obj.id = 1;
-        fs.appendFileSync(this.file, JSON.stringify(obj));
-        return obj.id;
-      } else {
-        let lastIdCreated = data[data.length - 1].id;
-        obj.id = lastIdCreated + 1;
-        data.push(obj);
-        fs.writeFileSync(this.file, JSON.stringify(data), "UTF-8");
-        return obj.id;
-      }
-    } catch (error) {
-      if (error.code === "ENOENT") {
-        try {
-          obj.id = 1;
-          const newObjectList = [obj];
-          fs.writeFileSync(
-            this.file,
-            JSON.stringify(newObjectList),
-            "UTF-8"
-          );
-          return obj.id;
-        } catch (error) {
-          throw error;
-        }
-      }
-      throw error;
-    }
-  }
-  getById(id) {
-    try {
-      const data = JSON.parse(fs.readFileSync(this.file, "UTF-8")).filter(
-        (item) => item.id === id
-      );
-      if (data.length === 0) {
-        return null;
-      } else {
-        return data;
-      }
-    } catch (error) {
-      throw error;
-    }
-  }
+	constructor(nombreArchivo) {
+		this.nombreArchivo = nombreArchivo;
+		this.content = [];
+	};
 
-  getAll() {
-    try {
-      const data = JSON.parse(fs.readFileSync(this.file, "UTF-8"));
-      return data;
-    } catch (error) {
-      throw error;
-    }
-  }
+	async save(producto) {
+		try {
+			this.content.push({
+				title: producto.title,
+				price: producto.price,
+				thumbnail: producto.thumbnail,
+				id: this.content.length + 1,
+			});
+			await fs.writeFile(this.nombreArchivo, JSON.stringify(this.content, null, 2));
+            return
+		} catch (error) {
+			console.log('No se pudo agregar un producto');
+            throw error
+		}
+	};
 
-  deleteById(id) {
-    try {
-        const oldData = fs.readFile(this.nombreArchivo, 'utf-8');
-        const dataParse = JSON.parse(oldData);
-        const producto = dataParse.find(prod => prod.id === id);
-        if(producto.id){
-        const newData = dataParse.filter(prod => prod.id !== id)
-        fs.writeFile(this.nombreArchivo, JSON.stringify(newData, null, 2));
-        const data = fs.readFile(this.nombreArchivo, 'utf-8');
-         return console.log(data)
-        } else
-         if (!producto.id){
-            return console.log('error');
-        }
-    } catch (error) {
-        throw error;
-    }
-  }
-  deleteAll() {
-    try {
-        fs.truncate(this.nombreArchivo,0);
-    } catch (error) {
-        throw error;
-    }
-  }
+    async getById(id){
+		try {
+			const data = await fs.readFile(this.nombreArchivo, 'utf-8');
+            const dataParse = JSON.parse(data)
+			const producto = dataParse.find(prod => prod.id === id);
+			if(producto){
+				console.log(`producto con el id ${id}:`);
+				console.log(producto)
+                return producto
+			} else
+			if(!producto){
+				console.log(`el producto con el id ${id} no existe`);
+			}
+			
+		} catch (error) {
+			console.log(`No se pudo encontrar producto con el id ${id}`);
+            throw error
+		}
+    };
+
+	//borrar por id
+	async deleteById(id){
+		try {
+			const oldData = await fs.readFile(this.nombreArchivo, 'utf-8');
+			const dataParse = JSON.parse(oldData);
+			const producto = dataParse.find(prod => prod.id === id);
+			if(producto.id){
+			const newData = dataParse.filter(prod => prod.id !== id)
+			await fs.writeFile(this.nombreArchivo, JSON.stringify(newData, null, 2));
+			const data = await fs.readFile(this.nombreArchivo, 'utf-8');
+			 return data
+			} else
+			 if (!producto.id){
+				return console.log('error');
+			}
+		} catch (error) {
+			console.log(`No se pudo borrar el producto con el id ${id}`);
+            throw error
+		}
+	};
+
+	getAll() {
+		try {
+			const data = fs.readFile(this.nombreArchivo, 'UTF-8');
+            return data
+		} catch (error) {
+			console.log('No se pudo leer el archivo');
+            throw error
+		}
+	};
 };
 
 const server = app.listen(PORT, () => {
@@ -95,21 +82,25 @@ const server = app.listen(PORT, () => {
 });
 server.on("error", (error) => console.log(error));
 
-const file = "./productos.txt";
+const file = "productos.txt";
+
 const productos = new Contenedor(file);
 
 function getRandomNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+app.get("/", (req, res) => {
+    res.status(200).send('<a href="/productos">Productos</a><br/><a href="/productoRandom">Producto Random</a>')
+  });
 
-app.get("/productos", (req, res) => {
-  const products = productos.getAll();
-  res.status(200).send(products);
+app.get("/productos", async(req, res) => {
+  const products = await productos.getAll();
+  res.status(200).send(JSON.parse(products))
 });
 
-app.get("/productoRandom", (req, res) => {
-    let todosLosProductos = productos.getAll();
-    const randomIndex = getRandomNum(0, todosLosProductos.length++);
-    const productoRandom = todosLosProductos[randomIndex];
+app.get("/productoRandom", async(req, res) => {
+    let todosLosProductos = await productos.getAll();
+    const randomIndex = getRandomNum(0, JSON.parse(todosLosProductos).length++);
+    const productoRandom = JSON.parse(todosLosProductos)[randomIndex];
     res.status(200).send(productoRandom)
 });
